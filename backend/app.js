3d916b9e9a07c404e319837ae4a0347cc98ecba0
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-07-28 23:20:21 +0530

    routes added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
-
+require("dotenv").config();
+const connectDB = require("./db/connect");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
-
-app.listen(3000, () => {
-  console.log("Listening at port 3000...");
+app.use(express.json());
+app.use("/", authRoutes);
+app.use("/", (req, res) => {
+  res.send("hi we are here");
 });
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
