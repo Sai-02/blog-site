63a3fbbf777df1c6ef7372d71bebb2a5b844dab1
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-07-30 21:15:47 +0530

    cors allow

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,14 @@ require("dotenv").config();
 const connectDB = require("./db/connect");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use("/", authRoutes);
 app.use("/", (req, res) => {
