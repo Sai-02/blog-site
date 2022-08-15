592ce59eae8389b5e7b545564436c6217a93fbda
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-08-15 19:37:24 +0530

    auth apis updated

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,51 +1,61 @@
 const User = require("../models/user.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const registerUser = async (req, res) => {
   const { name, password, email } = req.body;
   if (!name) return res.status(400).json({ msg: "Name is missing" });
   if (!email) return res.status(400).json({ msg: "Email is missing" });
   if (!password) return res.status(400).json({ msg: "Password is missing" });
-
-  let mailExists = false;
-  await User.findOne({ email: email })
-    .then((mail) => {
-      if (mail) mailExists = true;
-    })
-    .catch((e) => {
-      console.log(e);
+  try {
+    let mailExists = false;
+    await User.findOne({ email: email })
+      .then((mail) => {
+        if (mail) mailExists = true;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    if (mailExists) {
+      return res.status(409).json({ msg: "User email already exists" });
+    }
+    const user = await User.create({
+      name: name,
+      password: password,
+      email: email,
+      salt: "",
     });
-  if (mailExists) {
-    return res.status(409).json({ msg: "User email already exists" });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    user.salt = salt;
+    await user.save();
+    const accessToken = jwt.sign(user.toJSON(), process.env.TOKEN_KEY);
+    return res.json({ accessToken });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ msg: "Internal Server Error" });
   }
-  const user = await User.create({
-    name: name,
-    password: password,
-    email: email,
-  });
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-  await user.save();
-  console.log(user);
-  return res.json(user);
 };
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  let { email, password } = req.body;
   if (!email || !password)
     return res.status(400).json({ msg: "Email or password is not present !!" });
   try {
-    const user = await User.findOne({ email, password });
+    const user = await User.findOne({ email });
+    const salt = user.salt;
+    const hashPassword = await bcrypt.hash(password, salt);
     if (!user)
       return res.status(401).json({
         msg: "User not found !!",
       });
-    return res.status(200).json({
-      user,
-      msg: "Login Successful !!",
-    });
+    if (user.password !== hashPassword)
+      return res.status(403).json({ msg: "Password is incorrect" });
+    const accessToken = jwt.sign(user.toJSON(), process.env.TOKEN_KEY);
+    return res.json({ accessToken });
   } catch (e) {
+    console.log(e);
     return res.status(400).json({
       msg: "An error occured",
-      error:e
+      error: e,
     });
   }
 };
